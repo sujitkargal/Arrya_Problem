f448541e57b840234185b316364d8a14ea0c71b0
sujit kargal <sujitkargal0@gmail.com>
2025-01-26 11:48:41 +0530

    sun, 26 jan

---
+++ Arrya Problem/Show_even_No.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrya_Problem
+{
+    class Show_even_No
+    {
+        public static void Main(string[] args)
+        {
+        int[] a = new int[10];
+
+         Console.WriteLine("enter number len(10) ");
+            for (int i = 0; i < a.Length; i++) 
+            {
+                a[i] =  int.Parse(Console.ReadLine());
+             
+            }
+
+            Console.WriteLine("even numbers are");
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                   
+                  Console.WriteLine(a[i]);
+
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ Arrya Problem/sum_array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrya_Problem
+{
+    internal class sum_array
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] a = new int[5];
+
+            Console.WriteLine("enter a numbers to sum ");
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Addition of array is");
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum = a[i] + sum;
+                
+            }
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
